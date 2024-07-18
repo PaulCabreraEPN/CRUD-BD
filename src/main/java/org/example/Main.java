742ff663e4210409d53cae539472b219f6a862f7
1-21-01-E043-10\POO <paul.cabrera@epn.edu.ec>
2024-07-18 08:07:48 -0500

    V1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,47 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String url="jdbc:mysql://localhost:3306/Lista_estudiantes";
+        String user="root";
+        String passw="123456";
+        PreparedStatement ps = null;
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url,user,passw);
+            System.out.println("Conectado com sucesso");
+            //Prepara la sentencia SQL
+            String sql="UPDATE estudiantes SET b1 = ? where cedula = ?";
+            ps = connection.prepareStatement(sql);
+            //Seteamos los valores de la sentencia previa
+            ps.setInt(1,20);
+            ps.setString(2,"6000263486");
+            ps.executeUpdate();
+            int a = ps.getUpdateCount();
+            System.out.println("Tiene " + a + " lineas actualizadas");
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }finally {
+            //Cerramos la conección
+            try {
+                if (ps!=null){
+                    ps.close();
+                }
+                if (connection!=null){
+                    connection.close();
+                }
+            }catch (SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 }
